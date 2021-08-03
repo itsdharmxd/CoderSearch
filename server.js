4b41b7232e2b57dcbcdc4dc19406c7795830dc11
Dharmesh Upadhyay <udharmesh61@gmail.com>
2021-08-03 10:17:27 +0530

    for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-
+const path =require('path')
 
 
 const app = express();
@@ -49,6 +49,12 @@ mongoose.connect(db, {
 }).then(() => console.log("DB connected")).catch(err => console.log(err));
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 
